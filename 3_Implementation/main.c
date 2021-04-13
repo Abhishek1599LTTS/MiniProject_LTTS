878d29d37a17cde04c5293edb5f2465e41a75894
abhi <abhishek.1599@gmail.com>
2021-04-13 15:34:35 +0530

    Financial Claculator

---
+++ 3_Implementation/main.c
@@ -0,0 +1,30 @@
+#include "calculator.h"
+#include<stdio.h>
+#include <stdlib.h>
+
+int main (){
+ 
+    line();
+    printf("\n\t\tMonthly Financial Calculator\n");
+    line();
+
+    printf("\n");
+     
+    input();
+
+    providentfund();
+    calculate();
+
+    system("cls");
+
+    display();
+
+    message();
+
+    printf("\n");
+    printf("\n");
+
+    return 0;
+
+}
+
